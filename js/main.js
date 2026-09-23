78aec540a84cdba7bcd0c9620b052b7402167ddc
Didier alexis Cuan Najas <didiernajas2006@gmail.com>
2026-09-23 00:03:15 -0500

    pantallas responsivas y arreglo de foto y header

--- js/main.js
+++ js/main.js
@@ -158,13 +158,13 @@ function initPage() {
     if (typeof particlesJS !== 'undefined') {
         particlesJS('particles-js', {
             particles: {
-                number: { value: 80, density: { enable: true, value_area: 800 } },
-                color: { value: '#ebe7ef' },
+                number: { value: 150, density: { enable: true, value_area: 800 } },
+                color: { value: '#ecec0a' },
                 shape: { type: 'circle' },
-                opacity: { value: 0.5, random: false },
+                opacity: { value: 0.3, random: false },
                 size: { value: 3, random: true },
-                line_linked: { enable: true, distance: 150, color: '#52b3e0', opacity: 0.4, width: 1 },
-                move: { enable: true, speed: 2, direction: 'none', random: false, straight: false }
+                line_linked: { enable: true, distance: 150, color: '#fcfc07', opacity: 0.7, width: 1 },
+                move: { enable: true, speed: 1, direction: 'none', random: false, straight: false }
             }
         });
     }
